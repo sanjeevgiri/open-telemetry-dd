c8849e41a1e62599c510b57d2281401e48954940
Sanjeev Giri <sgirism@gmail.com>
2023-01-25 17:23:35 -0500

    initial

---
+++ otel-server/otel-server/src/main/kotlin/com/colorcodecanvas/Application.kt
@@ -0,0 +1,7 @@
+package com.colorcodecanvas
+
+import io.micronaut.runtime.Micronaut.run
+fun main(args: Array<String>) {
+	run(*args)
+}
+
---
+++ otel-server/otel-server/src/test/groovy/com/colorcodecanvas/OtelServerSpec.groovy
@@ -0,0 +1,19 @@
+package com.colorcodecanvas
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import jakarta.inject.Inject
+
+@MicronautTest
+class OtelServerSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
